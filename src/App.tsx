6588ad951a03dca8d0a2c48eff5cca664f04a1ba
Emma <yongjie.wu@outlook.com>
2022-12-05 17:55:55 +0800

    add eslint&editorconfig&prettierrc&lintStaged&husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import "./App.css";
-import Button from "@mui/material/Button";
-import { LineChart } from "./components/LineChart";
-import { Data } from "./utils/Data";
-import { Chart, registerables } from 'chart.js';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import Button from '@mui/material/Button'
+import { LineChart } from './components/LineChart'
+import { Data } from './utils/Data'
+import { Chart, registerables } from 'chart.js'
 
-
-Chart.register(...registerables);
+Chart.register(...registerables)
 
 function App() {
   const [chartProps] = useState({
@@ -14,16 +14,16 @@ function App() {
       labels: Data.map((data) => data.year),
       datasets: [
         {
-          label: "Users Gained ",
+          label: 'Users Gained ',
           data: Data.map((data) => data.userGain),
           backgroundColor: [
-            "rgba(75,192,192,1)",
-            "#ecf0f1",
-            "#50AF95",
-            "#f3ba2f",
-            "#2a71d0",
+            'rgba(75,192,192,1)',
+            '#ecf0f1',
+            '#50AF95',
+            '#f3ba2f',
+            '#2a71d0',
           ],
-          borderColor: "black",
+          borderColor: 'black',
           borderWidth: 2,
         },
       ],
@@ -32,20 +32,20 @@ function App() {
       plugins: {
         title: {
           display: true,
-          text: "Users Gained between 2016-2020",
+          text: 'Users Gained between 2016-2020',
         },
         legend: {
           display: false,
         },
       },
     },
-  });
+  })
   return (
     <div className="App">
       <Button variant="contained">Contained</Button>
       <LineChart chartProps={chartProps} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,22 +1,21 @@
-// import React from "react";
-import { ChartData, ChartOptions } from "chart.js";
-import { Line } from "react-chartjs-2";
-
+import React from 'react'
+import { ChartData, ChartOptions } from 'chart.js'
+import { Line } from 'react-chartjs-2'
 
 interface LineProps {
-  options: ChartOptions<'line'>;
-  data: ChartData<'line'>;
+  options: ChartOptions<'line'>
+  data: ChartData<'line'>
 }
 
 interface Props {
   chartProps: LineProps
 }
 
-export const LineChart = (props: Props ) => {
+export const LineChart = (props: Props) => {
   return (
-    <div className="chart-container" style={{ width: "80%" }}>
-      <h2 style={{ textAlign: "center" }}>Line Chart</h2>
+    <div className="chart-container" style={{ width: '80%' }}>
+      <h2 style={{ textAlign: 'center' }}>Line Chart</h2>
       <Line {...props.chartProps} />
     </div>
-  );
-};
+  )
+}
